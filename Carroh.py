8e36834596d8ad6079b35c66d46de632ab7f4f96
enzognocalves <142853857+enzognocalves@users.noreply.github.com>
2023-10-16 21:59:28 -0300

    Add files via upload

---
+++ Carroh.py
@@ -0,0 +1,17 @@
+#Herança
+class Carro:
+    def __init__(self,Carro,moto,descricao):
+        self._Carro = Carro
+        self._moto = moto
+        self._descricao=descricao
+    def emitirsom(self):
+        print(f'{self._Carro} faz um som')
+
+    def emitirsom(self):
+        print(f'{self._moto} ram ram')
+
+  def emitirsom(self):
+        print(f'{self._descricao} ou e moto ou carro')
+
+ def emitirsom(self):
+        print(f'{self._descricao} ou e moto ou carro')
